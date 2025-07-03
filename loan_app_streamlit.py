b8e2575aeccac55f7eac1a88dbd18b313647a0b6
krishnadevda4170 <krishnadevda39@gmail.com>
2025-07-03 14:23:53 +0530

    Update loan_app_streamlit.py

--- loan_app_streamlit.py
+++ loan_app_streamlit.py
@@ -3,7 +3,7 @@ import numpy as np
 import pickle
 
 # Load the model
-with open(r'C:\Users\krishna devda\OneDrive\Desktop\Loan_prediction_model\model\loan_prediction.pkl', 'rb') as file:
+with open('model\\loan_prediction.pkl', 'rb') as file:
     model = pickle.load(file)
 
 # Streamlit UI
